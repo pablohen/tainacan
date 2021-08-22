a75e550503cf8d72ff71a91b4af00d95478020a0
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-21 21:01:41 -0300

    update description

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,6 +1,7 @@
 const site = `https://${process.env.VERCEL_URL}`;
 const siteName = 'Tainacan - Casos de Uso';
-const description = 'Portfólio dos casos de uso do Tainacan';
+const description =
+  'Projeto pessoal para agregar conteúdo de diversos sites que estão usando a ferramenta Tainacan, disponível para WordPress.';
 
 const seoConfig = {
   defaultTitle: siteName,
