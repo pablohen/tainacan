c2758d4ac6551873babb70cfa0a826eafd341dc2
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-30 09:22:55 -0300

    config theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider attribute="class">
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 export default MyApp;
