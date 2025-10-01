438f07bd155400138db61c23aec4df5d1d8e9d75
Pablo Henrique <pablo_hen@hotmail.com>
2025-10-01 12:13:13 -0300

    feat: remove loading between pages

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "@/components/ui/spinner";
-
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Spinner size={48} />
-    </div>
-  );
-}
